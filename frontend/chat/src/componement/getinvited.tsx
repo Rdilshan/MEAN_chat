b02df2b8d837d4d4ca216720cb6b6603106aac66
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-29 07:36:33 +0530

    for geting your code

---
+++ frontend/chat/src/componement/getinvited.tsx
@@ -0,0 +1,37 @@
+export default function Getinvited() {
+  return (
+    <>
+      <div className="col-sm-8 conversation">
+        <div className="row showingLink " >
+          <div className="row message-previous " >
+            <div className="col-sm-12 previous">
+              <a id="ankitjain28">Getting your code</a>
+
+              <div className="clipboard-example align-items-center w-50">
+                <div className="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value="39XMhLRH54kNyYHfiUWC8xrz5HRmZQxL7f"
+                  />
+                  <button
+                    id="btn01"
+                    className="btn btn-secondary copybtn"
+                    type="button"
+                    data-clipboard-demo=""
+                    data-clipboard-target="#in01"
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="bottom"
+                    title="Copy to Clipboard"
+                  >
+                    Copy
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
