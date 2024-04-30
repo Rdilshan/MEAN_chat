28ab9b5b9764ef7ab5528300d3ebb2604235ca90
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-30 06:05:55 +0530

    headers: {           "Content-Type": "application/json",         } set,

--- frontend/chat/src/componement/member.tsx
+++ frontend/chat/src/componement/member.tsx
@@ -6,29 +6,35 @@ import Side from "./side";
 import axios from "axios";
 
 export default function Chatsmember() {
-
   useEffect(() => {
-    axios.get("http://localhost:3000/api/user/home").then((res) => {
-      if(res.data.data == "not login"){
-        window.location.href = '/signIn';
-      }else{
-        console.log(res.data.data);
-      }
+    checkinglogin();
+  });
+  const checkinglogin = () => {
+    axios
+      .get("http://localhost:3000/api/user/home", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      })
+      .then((res) => {
+        if(res.data.data == "not login"){
+          window.location.href = '/signIn';
+        }else{
+          console.log(res.data.data);
+        }
 
-    })
-  })
+      });
+  };
   return (
     <div>
       <div className="app">
         <div className="row app-one">
-
           <Side />
 
           <Conversation />
           <Getinvited />
           <Joinmember />
-
-
         </div>
       </div>
     </div>
