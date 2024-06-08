4a042b6d68161ebf5d238f875aaaa398b11d9522
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-08 14:25:43 +0530

    chat adding

--- Backend/controller/chatController.js
+++ Backend/controller/chatController.js
@@ -1,37 +1,53 @@
 const express = require("express");
-const chat = require("../model/chat")
+const mongoose = require("mongoose");
+const chat = require("../model/chat");
+const User = require("../model/user");
 
-const chatcreate = (req,resp)=>{
-    const userid = req.user._id
-    const requestData = req.body;
+const chatcreate = async (req, resp) => {
+  try {
+    const userid = req.user._id;
+    const requestData = req.body.refid;
+    if (!mongoose.Types.ObjectId.isValid(requestData)) {
+      return resp.status(400).json({ error: "Invalid refid format" });
+    }
+    var id = requestData;
+    const finduser = await User.findById(id);
 
-    const newChat = new chat({
-        users: [...requestData.users, userId],
-        createdAt: new Date() 
+    if (finduser) {
+        
+      const newChat = new chat({
+        users: [requestData, userid],
+        createdAt: new Date(),
+      });
+
+      newChat
+        .save()
+        .then((savedChat) => {
+          resp.status(201).json({ msg: savedChat });
+        })
+        .catch((error) => {
+          resp.status(500).json({ error: error.message });
+        });
+    }
+  } catch (error) {
+    console.error("Error finding user:", error);
+    resp.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getchats = (req, resp) => {
+  const userid = req.user._id;
+  chat
+    .find({ users: userid })
+    .then((chats) => {
+      resp.status(200).json(chats);
+    })
+    .catch((err) => {
+      resp.status(500).json({ error: err.message });
     });
-
-    newChat.save()
-        .then(savedChat => {
-            resp.status(201).json({msg:savedChat});
-        })
-        .catch(error => {
-            resp.status(500).json({ error: error.message });
-        });
-}
-
-const getchats = (req,resp)=>{
-    const userid = req.user._id
-    chat.find({ users: userid })
-        .then(chats => {
-
-            resp.status(200).json(chats);
-        })
-        .catch(err => {
-            resp.status(500).json({ error: err.message });
-        });
-}
+};
 
 module.exports = {
-    chatcreate,
-    getchats
+  chatcreate,
+  getchats,
 };
